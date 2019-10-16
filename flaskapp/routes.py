535e74b477fa9d1357d39ba08a66e41ad9b8e789
ValentynZal <valentin11091994@gmail.com>
2019-10-16 10:58:38 +0300

    decided to split routes

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import paralleldots
+from math import floor
 from datetime import datetime
 from flask import (
     Blueprint, g, redirect, render_template, request, url_for, flash, current_app
@@ -21,7 +22,7 @@ def text_to_emotion(text):
     prior = max(arr)
     for k, v in dictionary['emotion'].items():
         if v == prior:
-            return k
+            return f'{k}: {floor(v*100)}%'
 
 def timestamp_converter(timestamp):
     dt = datetime.utcfromtimestamp(timestamp)
@@ -38,7 +39,7 @@ def csv_extracter(filename):
             city = line[' city']
             emotion = text_to_emotion(line[' text'])
             month, time = timestamp_converter(int(line[' timestamp']))
-        return sex, city, emotion, month, time
+        yield sex, city, emotion, month, time
  
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -53,18 +54,30 @@ def index():
             return redirect(request.url)
         if file:
             file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], file.filename))        
-        sex, city, emotion, month, poll_time = csv_extracter(file.filename)
+        gen = csv_extracter(file.filename)
         db = get_db()
-        db.execute(
-                'INSERT INTO poll (sex, city, emotion, month, poll_time)\
-                 VALUES (?, ?, ?, ?, ?)',
-                (sex, city, emotion, month, poll_time)
-        )
-        db.commit()
-        print("saved")
+        for row in gen:
+            db.execute(
+                '''INSERT INTO poll (sex, city, emotion, month, poll_time)
+                VALUES (?, ?, ?, ?, ?)''',
+                (row[0], row[1], row[2], row[3], row[4])
+            )
+            db.commit()
+        # print(db.execute("SELECT * FROM poll WHERE id = 1").fetchone().commit())
+        db.close()
+        # print("saved")
         # return redirect(url_for('page'))
     return render_template('page.html')
 
+@bp.route('/choose', methods=['POST', 'GET'])
+def choose():
+    form = request.form
+    print(form)                         
+    return redirect(url_for('index'))
+
+@bp.route('/download', methods=['POST', 'GET'])
+def download():
+    pass
 
 
 
